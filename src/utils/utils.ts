4c11d4791e4bdd2be61c0bb0ace57513aec1f0b1
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-22 14:57:43 +0200

    refactor: reducer of the useReducer hook

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -13,35 +13,43 @@ export const ACTIONS: {
 export function reducer(todos: Todo[], action: CounterAction): Todo[] {
   switch (action.type) {
     case ACTIONS.ADD_TODO:
-      if (todos && action.payload.newTodo)
-        return [action.payload.newTodo, ...todos];
-      return todos;
+      return addTodo(todos, action);
 
     case ACTIONS.REMOVE_TODO: {
-      const indexOfElem = todos.findIndex(
-        (todo) => todo.id === action.payload.id
-      );
-      if (indexOfElem === -1) return todos;
-      return [
-        ...todos.slice(0, indexOfElem),
-        ...todos.slice(indexOfElem + 1, todos.length),
-      ];
+      return completeTodo(todos, action);
     }
 
     case ACTIONS.COMPLETE_TODO: {
-      const indexOfElem = todos.findIndex(
-        (todo) => todo.id === action.payload.id
-      );
-      if (indexOfElem === -1) return todos;
-      const todo = todos[indexOfElem];
-      return [
-        ...todos.slice(0, indexOfElem),
-        { id: todo.id, title: todo.title, completed: !todo.completed },
-        ...todos.slice(indexOfElem + 1, todos.length),
-      ];
+      return removeTodo(todos, action);
     }
 
     default:
       return todos;
   }
+}
+
+function addTodo(todos: Todo[], action: CounterAction) {
+  if (todos && action.payload.newTodo)
+    return [action.payload.newTodo, ...todos];
+  return todos;
+}
+
+function completeTodo(todos: Todo[], action: CounterAction) {
+  const indexOfElem = todos.findIndex((todo) => todo.id === action.payload.id);
+  if (indexOfElem === -1) return todos;
+  return [
+    ...todos.slice(0, indexOfElem),
+    ...todos.slice(indexOfElem + 1, todos.length),
+  ];
+}
+
+function removeTodo(todos: Todo[], action: CounterAction) {
+  const indexOfElem = todos.findIndex((todo) => todo.id === action.payload.id);
+  if (indexOfElem === -1) return todos;
+  const todo = todos[indexOfElem];
+  return [
+    ...todos.slice(0, indexOfElem),
+    { id: todo.id, title: todo.title, completed: !todo.completed },
+    ...todos.slice(indexOfElem + 1, todos.length),
+  ];
 }
